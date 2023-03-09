d17b5f506fd129ad4b6a32aec5c663d12ff8827d
JustANipple <prandisamuel99@gmail.com>
2023-03-09 12:09:55 +0100

    modified:   index.html 	modified:   script.js 	modified:   style.css

---
+++ script.js
@@ -0,0 +1,29 @@
+const email = document.querySelector("#typeEmail");
+const button = document.querySelector(".send-email");
+const errorIcon = document.querySelector(".error-icon");
+const errorText = document.querySelector(".email-error");
+const regex = new RegExp("[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-z]+");
+let emailText = "";
+
+email.addEventListener("change", (event) => {
+    emailText = document.getElementById("typeEmail").value;
+    if(!regex.test(emailText)) {
+        email.style.border = "2px solid red";
+        errorIcon.style.visibility = "visible";
+        errorText.style.visibility = "visible";
+    } else {
+        email.style.border = "2px solid green";
+        errorIcon.style.visibility = "hidden";
+        errorText.style.visibility = "hidden";
+    }
+});
+
+button.addEventListener("click", (event) => {
+    if(regex.test(emailText)) {
+        window.location.reload();
+    } else {
+        email.style.border = "2px solid red";
+        errorIcon.style.visibility = "visible";
+        errorText.style.visibility = "visible";
+    }
+});
